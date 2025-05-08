24338b71173e444c60353e2498b2dd38c78b7ef6
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2025-05-08 16:18:29 +0600

    update

--- src/index.ts
+++ src/index.ts
@@ -43,3 +43,13 @@ class Car extends Vehicle {
     return `Model: ${this.model}`;
   }
 }
+
+
+function processValue(value: string | number): number{
+  if(typeof value == "string"){
+    return value.length
+  }
+  else{
+    return value * 2
+  }
+}
