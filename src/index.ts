f14065f8a9588ee2440834d8c39bedec2b7674b9
Md Monjur Bakth Mazumder <md.monjurmbm2001@gmail.com>
2025-05-08 15:12:10 +0600

    completed problem 2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-function formatString(input: string, toUpper: boolean = true): string{
-    return toUpper ? input.toUpperCase() : input?.toLocaleLowerCase()
+function formatString(input: string, toUpper?: boolean): string {
+  return toUpper === false ? input?.toLocaleLowerCase() : input.toUpperCase();
 }
 
+function filterByRating(
+  items: { title: string; rating: number }[]
+): { title: string; rating: number }[] {
+  return items.filter((item) => item.rating >= 4);
+}
